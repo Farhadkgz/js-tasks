0c0fb88f47bc5254c3cc607fe8a66ba415233131
Farhadkgz <javascriptkgz@gmail.com>
2023-07-18 20:46:57 +0600

    independent work

---
+++ 18-07/script.js
@@ -0,0 +1,19 @@
+let x = 7;
+
+if(x === 7){
+    alert('Верно')
+}else{
+    alert('Неверно');
+}
+
+// let a = 1;
+// let b = 3;
+
+// if(a <= 1){
+//     alert(a)
+// }
+// if else(b >= 3){
+//     alert(b)
+// }else{
+//     alert('Неверно')
+// }
