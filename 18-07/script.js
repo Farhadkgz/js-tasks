ccf9c15573b4ceb4eae10b99685609c3d3b96bd4
Farhadkgz <javascriptkgz@gmail.com>
2023-07-19 23:35:52 +0600

    update js-tasks

---
+++ 18-07/script.js
@@ -0,0 +1,32 @@
+// ---------------------SIGNIN----------------
+let who = prompt('Кто пришел?');
+let anOther = 'Я вас незнаю';
+let exit = 'Вход отменен';
+
+
+if(who === 'Admin'){
+    alert(password)
+}
+    else if(who !== 'Admin' && who !== null){
+    alert(anOther)
+}else{
+        alert(exit);
+ }
+
+// ---------------------PASSWORD----------------
+let password = prompt('Введите пароль:');
+let welcome = 'Добро пожаловать!';
+let passwordError = 'Пароль неверен';
+
+if(password === '123456'){
+    alert(welcome)
+}
+else if(password !== '123456' && password !== null){
+    alert(passwordError)
+}else{
+    alert(exit);
+}
+
+
+
+ 
