841c77ef2575dc1976261bbfea260f9ea16d8247
Farhadkgz <javascriptkgz@gmail.com>
2023-07-22 20:15:17 +0600

    update tasks

---
+++ 22-07/script.js
@@ -0,0 +1,73 @@
+// const countries = ['Kyrgyzstan', 5, true, {}, null];
+// console.log(countries[4]);
+
+// const numbers = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+
+// console.log(numbers.length);
+
+// numbers[0] = 'helo';
+
+// console.log(numbers);
+// console.log(numbers.length);
+
+// const cars = ['BMW', 'SUBARU', 'TOYOTA', 'TESLA'];
+
+// const deletedElem = cars.pop();
+
+// cars.push('MERS', 'HONDA');
+
+// const elem = cars.shift();
+// cars.unshift('KIA');
+
+// console.log(cars);
+
+             // --------------ЦИКЛЫ----------
+
+// for(let i = 0; i < 5; i++){
+//     console.log(i);
+// }
+
+/*
+1)
+let i = 0;
+0 < 5;
+console.log(i);
+
+2)
+i++; 1
+1 < 5; true
+console.log(i); 0
+
+3)
+i++; 2
+2 < 5; true
+console.log(i); 2
+
+4)
+i++; 3
+3 < 5; true
+console.log(i); 3
+
+5)
+i++; 4
+4 < 5; true
+console.log(i); 4
+
+3)
+i++; 5
+5 < 5; false
+Цикл завершвется!
+
+*/
+
+let cars = ['BMW', 'SUBARU', 'TOYOTA', 'TESLA'];
+
+for(let i = 0; i < cars.length; i++){
+    console.log(i);
+}
+
+
