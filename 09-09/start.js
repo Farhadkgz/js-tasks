734182378a20c357fa3255fc93d1899df5183d54
Farhadkgz <javascriptkgz@gmail.com>
2023-09-12 19:57:13 +0600

    Reload Auth_Project

---
+++ 09-09/start.js
@@ -0,0 +1,32 @@
+const submit = document.querySelector('.btn2')
+const student_exam_id = localStorage.getItem('student_exam_id');
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    localPost();
+})
+
+const base_url = 'http://51.250.97.89:8008';
+
+const objLocal = {
+    data: student_exam_id
+  };
+
+  async function localPost(){
+    const url = base_url + '/api/v1/exam/start?student_exam_id=' + student_exam_id;
+
+    const options = {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+
+    const response = await fetch(url, options);
+    if(response.ok){
+        const responseData = await response.json();
+    }
+  }
+
+
